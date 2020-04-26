f9a38ef0945c7e121b773fe600988a8dd4c2f23e
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-04-26 19:41:29 -0300

    fix: Routes
    Removendo o header

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,12 @@ import Home from "./pages/Home";
 import Timer from "./pages/Timer";
 
 function Routes() {
-    return (
-        <AppStack.Navigator>
-            <AppStack.Screen name="Home" component={Home} />
-            <AppStack.Screen name="Timer" component={Timer} />
-        </AppStack.Navigator>
-    )
-};
+  return (
+    <AppStack.Navigator screenOptions={{ headerShown: false }}>
+      <AppStack.Screen name="Home" component={Home} />
+      <AppStack.Screen name="Timer" component={Timer} />
+    </AppStack.Navigator>
+  );
+}
 
 export default Routes;
