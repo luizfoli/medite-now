28a04704379b967cf139de35b02040fea0407ce8
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-04-26 19:08:28 -0300

    feat: Routes
    Criando a classe de Rotas

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AppStack = createStackNavigator();
+
+import Home from "./pages/Home";
+import Timer from "./pages/Timer";
+
+function Routes() {
+    return (
+        <AppStack.Navigator>
+            <AppStack.Screen name="Home" component={Home} />
+            <AppStack.Screen name="Timer" component={Timer} />
+        </AppStack.Navigator>
+    )
+};
+
+export default Routes;
