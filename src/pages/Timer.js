2b212d99ee9bf49f6c4076f9954b3c4d2a4e7f3e
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-05-08 22:14:16 -0300

    fix: Creating the logic from Timer

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+
+    button: {
+        width: 72,
+        height: 72,
+        backgroundColor: "#5CA9B1",
+        borderRadius: 36,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+  });
+
+let currentInterval = undefined;
 
 export default function Timer() {
 
-    return (
-        <View>
+    const period = 60;
 
-        </View>
+    const [isPlay, setIsPlay] = useState(false);
+    const [seconds, setSeconds] = useState(period);
+
+    const count = () => {
+        setIsPlay(!isPlay);
+
+        if(isPlay) {
+            clearInterval(currentInterval); 
+        } else {
+            currentInterval = setInterval(() => {
+                setSeconds(state => state - 1);
+            }, 1000);
+        }
+    };
+
+    return (
+        <LinearGradient colors={["#fff", "#fff"]} style={styles.container}>
+            <Text>{seconds}</Text>
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={e => count()}>
+                <MaterialIcons name={isPlay ? "pause" : "play-arrow"} size={32} color="#fff"></MaterialIcons>
+            </TouchableOpacity>
+        </LinearGradient>
     );
 
 
